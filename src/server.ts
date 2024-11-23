e206b8c6006fb541853e5788eed56bfb6b6ff00c
Arthur Mendes <arthurmendesmartins0105@gmail.com>
2024-11-23 09:34:37 -0300

    refact: trying to fix deploy on render -> add back server listen.

--- src/server.ts
+++ src/server.ts
@@ -51,10 +51,10 @@ server.use("/report", ReportRouter);
 server.use("/transaction", TransactionRouter);
 server.use("/virtualStock", VirtualStockRouter);
 
-// server.listen(process.env.SERVER_PORT ?? 1805, () => {
-//   console.log(
-//     `Server is running on port: ${process.env.SERVER_PORT} \nAccess the server here: http://localhost:${process.env.SERVER_PORT ?? 1805}`
-//   );
-// });
+server.listen(process.env.SERVER_PORT ?? 1805, () => {
+  console.log(
+    `Server is running on port: ${process.env.SERVER_PORT} \nAccess the server here: http://localhost:${process.env.SERVER_PORT ?? 1805}`
+  );
+});
 
 export { server }
